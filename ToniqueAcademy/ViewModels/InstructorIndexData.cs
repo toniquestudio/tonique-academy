e5c7b3aa0b2b4ccf120a8a5fbe43aca1b84d46b6
Maxime Beaudoin <maxime@toniquestudio.com>
2017-02-27 16:21:41 -0500

    Project refactoring

---
+++ ToniqueAcademy/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ToniqueAcademy.Domain;
+
+namespace ToniqueAcademy.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
+
